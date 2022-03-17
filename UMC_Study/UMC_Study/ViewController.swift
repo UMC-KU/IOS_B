c9ec3e9bb119c63bebc4ada46fac0725ef889446
HyoWon Choi <81157265+wonniiii@users.noreply.github.com>
2022-03-17 19:34:37 +0900

    week1

---
+++ UMC_Study/UMC_Study/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  UMC_Study
+//
+//  Created by 맥프로 on 2022/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("TEST")
+    }
+
+
+}
+
